98462cb5b024dff98e73a2d709b526bea577095f
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2022-08-07 14:42:37 +0100

    Use iso string for time in logger
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/core/clients/logger.ts
+++ src/core/clients/logger.ts
@@ -1,4 +1,8 @@
 import { pino } from "pino";
 
 export const makeLogger = (namespace: string) =>
-  pino({ name: namespace, level: process.env.NODE_ENV === "production" ? "info" : "trace" });
+  pino({
+    name: namespace,
+    level: process.env.NODE_ENV === "production" ? "info" : "trace",
+    timestamp: () => `,"time": "${new Date().toISOString()}"`,
+  });
