7cd18651f2ae14ba697f881346399914d70af725
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2022-08-14 22:38:50 +0100

    Add koa session
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/admin/app.ts
+++ src/apps/admin/app.ts
@@ -1,7 +1,9 @@
+import config from "config";
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import render from "koa-ejs";
 import qs from "koa-qs";
+import session from "koa-session";
 import koaStatic from "koa-static";
 import path from "node:path";
 import url from "node:url";
@@ -10,6 +12,7 @@ import router from "#src/apps/admin/router.js";
 
 export const app = () => {
   const koa = new Koa();
+  koa.keys = config.get("apps.admin.keys");
 
   qs(koa);
   render(koa, {
@@ -20,6 +23,7 @@ export const app = () => {
     debug: false,
   });
 
+  koa.use(session({ ...config.get("apps.admin.session") }, koa));
   koa.use(bodyParser());
   koa.use(koaStatic(path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), "../../../public")));
   koa.use(router.middleware());
