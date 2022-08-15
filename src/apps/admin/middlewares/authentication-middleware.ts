2e787e72a4faa8c7c4969acc9deda10a93bfa164
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2022-08-15 11:34:02 +0100

    Add authentication middleware
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ src/apps/admin/middlewares/authentication-middleware.ts
@@ -0,0 +1,18 @@
+import { RouterContext } from "@koa/router";
+import { Next } from "koa";
+
+import { userRepository } from "#src/entities/user/repositories/user-repository.js";
+
+export async function authenticationMiddleware(context: RouterContext, next: Next) {
+  if (!context.session.user?.email) {
+    context.throw(401, "Not authenticated");
+  }
+
+  const user = await userRepository.getUserByEmail(context.session.user.email);
+
+  if (!user) {
+    context.throw(401, "Not authenticated");
+  }
+
+  await next();
+}
