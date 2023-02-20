dc11cbd17d935b6f720bdd4c7f532c024b91ace5
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-02-20 21:42:44 +0000

    Fix type on sql helper function
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/database/core/utils/sql.ts
+++ src/database/core/utils/sql.ts
@@ -27,7 +27,7 @@ export const gte = <T = any>(prop: Query, value: T[keyof T] | Query) => condExp<
 export const lte = <T = any>(prop: Query, value: T[keyof T] | Query) => condExp<T>(prop, "lte", value);
 export const btw = <T = any>(prop: Query, from: T[keyof T] | Query, to: T[keyof T] | Query) =>
   condExp<T>(prop, "btw", sql`$${isQuery(from) ? from : sql`${from}`} and $${isQuery(to) ? to : sql`${to}`}`);
-export const lk = <T = any>(prop: Query, value: Query) => condExp<T>(prop, "lk", value);
+export const lk = <T = any>(prop: Query, value: string | Query) => condExp<T>(prop, "lk", value as any);
 export const iin = <T = any>(prop: Query, ...value: Array<T[keyof T] | Query>) =>
   condExp<T>(prop, "in", sql`($${join(value)})`);
 export const exts = <T = any>(prop: Query, value: Query) => condExp<T>(prop, "exts", sql`($${value})`);
