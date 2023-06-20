77b7536493f691e2f0ac874e81edc8a6b4c07902
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-06-20 22:14:50 +0100

    Add loading page
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/http/admin/public/js/main.js
+++ src/apps/http/admin/public/js/main.js
@@ -1,25 +1,13 @@
-import { StrictMode, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { useDarkMode } from "usehooks-ts";
 
 import html from "./common/html.js";
 import router from "./pages/mod.js";
 
-const Root = () => {
-  const { isDarkMode } = useDarkMode();
-
-  useEffect(() => {
-    globalThis.document.documentElement.dataset.bsTheme = isDarkMode ? "dark" : "light";
-  }, [isDarkMode]);
-
-  return html``;
-};
-
 createRoot(globalThis.document.querySelector("#app")).render(
   html`
     <${StrictMode}>
-      <${Root} />
       <${RouterProvider} router=${router} />
     <//>
   `,
