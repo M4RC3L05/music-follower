82df1789bc65a00e5341345b1a025cba5f08c3f7
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-06-24 15:32:34 +0100

    Make esmsh be proxied form the server and allow to customize esm sh endpoin
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/http/admin/app.ts
+++ src/apps/http/admin/app.ts
@@ -2,12 +2,20 @@ import Koa from "koa";
 import basicAuth from "koa-basic-auth";
 import config from "config";
 import koaStatic from "koa-static";
+import proxy from "koa-proxies";
 
 export const app = () => {
   const koa = new Koa();
 
   koa.use(basicAuth({ ...config.get("apps.admin.basicAuth") }));
   koa.use(koaStatic("./src/apps/http/admin/public"));
+  koa.use(
+    proxy("/deps", {
+      target: config.get("apps.admin.esmsh"),
+      rewrite: (path) => path.replace("/deps", ""),
+      changeOrigin: true,
+    }),
+  );
 
   return koa;
 };
