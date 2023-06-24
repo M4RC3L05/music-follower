fb74d95e652fc81e89f9c962f9639ede6cd9f914
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-06-24 15:38:39 +0100

    Add feed favicon
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/http/feed/app.ts
+++ src/apps/http/feed/app.ts
@@ -1,10 +1,12 @@
 import Koa from "koa";
+import favicon from "koa-favicon";
 
 import { feedMiddleware } from "#src/apps/http/feed/middlewares/feed-middleware.js";
 
 export const app = () => {
   const koa = new Koa();
 
+  koa.use(favicon("./src/apps/http/feed/favicon.ico"));
   koa.use(feedMiddleware);
 
   return koa;
