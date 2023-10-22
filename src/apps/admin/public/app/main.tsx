61fede8f35b82c10285ef462e1ee9197069ff47c
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-10-22 14:11:44 +0100

    Improve xo config
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/admin/public/app/main.tsx
+++ src/apps/admin/public/app/main.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import { StrictMode } from "react";
-// eslint-disable-next-line n/file-extension-in-import
 import { createRoot } from "react-dom/client";
 
 import router from "./pages/mod.js";
